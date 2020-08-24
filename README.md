e7d4499a78871c0dd0d41b3fb1cb089f4e5b5dcd
Robert M. Nill <rmnill202mail@gmail.com>
2020-08-24 01:46:05 -0400

    Initial website

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# web-brainstorming
+# Web Brainstorming
+Place for me to experiment with some UI ideas without dedicating an entire website to those ideas.
+
+## Installation & Running
+- `npm install` and `npm run serve`
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL, // Needs to be set since the vue.config.js publicpath has been changed according to: 
+  routes,
+});
+
+export default router;
