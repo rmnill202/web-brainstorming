235380f148468da414aa2d03997c4ea1f8ed1b75
Robert M. Nill <rmnill202mail@gmail.com>
2020-09-06 00:00:10 -0400

    Better rotation example

--- src/experiments/RotateHover.vue
+++ src/experiments/RotateHover.vue
@@ -5,7 +5,14 @@
 
     <div>No debounce, rotate child div of wrapper.</div>
     <a class="parent-to-card" @mousemove="mouseCardChild($event)" @mouseleave="clearChildCard($event)">
-      <div class="rounded-card-child" :style="childCardStyle">Child Div</div>
+      <div class="rounded-card-child" :style="childCardStyle">
+        <div class="card-highlight" :style="highlightStyle"></div>
+        <div style="position: absolute; right: 10px; font-size: 28px">CC Company</div>
+        <div style="width: 60px; height: 45px; background-color: #e4e4a0; border-radius: 5px; position: absolute; left: 50px; bottom: 95px"/>
+        <div style="position: absolute; bottom: 60px; font-size: 24px; left: 50px">1111 2222 3333 4444</div>
+        <div style="position: absolute; bottom: 30px; left: 145px; font-weight: 300">12/24</div>
+        <div style="position: absolute; bottom: 10px; left: 20px; font-weight: 300">First Last</div>
+      </div>
     </a>
   </div>
 </template>
@@ -26,6 +33,7 @@ export default {
       scrollAnimWaiting: false,
 
       childnx: 0, childny: 0,
+      hlx: 0, hly: 0,
       
     }
   },
@@ -34,6 +42,12 @@ export default {
       return {
         '--childnx': this.childnx,
         '--childny': this.childny,
+      };
+    },
+    highlightStyle() {
+      return {
+        '--hlx': this.hlx,
+        '--hly': this.hly,
       };
     }
   },
@@ -81,6 +95,8 @@ export default {
       // e.target.style = `transform: rotateY(${rx}deg) rotateX(${ry}deg);`;
       this.childnx = `${rx}deg`
       this.childny = `${ry}deg`;
+      this.hlx = `${-rx * 3.5}px`
+      this.hly = `${ry  * 1.5}px`;
       this.scrollAnimWaiting = false;
     },
     // Mouseleave would be better in this case, since I don't wanna track events thrown by leaving child divs
@@ -95,6 +111,8 @@ export default {
       window.requestAnimationFrame(() => {
         this.childnx = `0deg`;
         this.childny = `0deg`;
+        this.hlx = `0`;
+        this.hly = `0`;
       });
     },
     lerp(t, a, b) {
@@ -112,18 +130,20 @@ export default {
   min-height: 200px;
   min-width: 400px;
   border-radius: 3px;
-  background-color: rgb(82, 75, 75);
+  background-color: #524b4b;
 
   transition: transform 0.15s ease;
 }
 .rounded-card-child {
+  pointer-events: none;
   display: inline-block;
+  position: relative;
   color: white;
   padding: 10px;
   min-height: 200px;
   min-width: 400px;
   border-radius: 3px;
-  background-color: rgb(82, 75, 75);
+  background-color: #853d3d;
 
   transition: transform 0.1s ease;
   transform: rotateY(var(--childnx)) rotateX(var(--childny));
@@ -133,11 +153,23 @@ export default {
   cursor: pointer;
 }
 .parent-to-card:active .rounded-card-child {
-  background-color: rgb(133, 61, 61);
+  background-color: #853d3d;
   transform: rotateY(var(--childnx)) rotateX(var(--childny)) scale(0.9);
 }
 .perspective-parent {
   perspective: 750px;
+}
+
+
+.card-highlight {
+  position: absolute;
+  left: 100px;
+  width: 200px;
+  height: 200px;
+  background-color: #ffffff09;
+  clip-path: circle();
+  transform: translateX(var(--hlx)) translateY(var(--hly));
+  transition: transform 0.1s ease;
 }
 
 </style>
